9a474d48fe09bd6eb38df1b43401295fd4fc3a49
nihalkumar33 <nihal7u@gmail.com>
2025-04-01 23:58:06 +0530

    Implemented filestructure initial right-sidebar

---
+++ components/PodcastCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PodcastCard = ({
+    imgUrl, description, podcastId, title
+}: {
+    imgUrl: string
+    description: string
+    podcastId: number
+    title: string
+}) => {
+  return (
+    <div>PodcastCard</div>
+  )
+}
+
+export default PodcastCard
