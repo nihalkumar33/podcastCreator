ad6e7f4054b665c10962821bdfdbfccc7cb0610e
nihalkumar33 <nihal7u@gmail.com>
2025-04-02 21:06:42 +0530

    Implemented clerk and some convex

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+// Create a route matcher for public routes (e.g., sign-in, sign-up, and the homepage)
+const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up', '/']);
+
+export default clerkMiddleware((auth, req) => {
+  // Protect non-public routes
+  if (!isPublicRoute(req)) {
+    auth().protect(); 
+  }
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
